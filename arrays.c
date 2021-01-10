3a0f159b05c776aa9fab7d8d00548a2101ed9881
Abhishek Dilip Dhok <53620210+abhishek143-tech@users.noreply.github.com>
2021-01-10 03:58:42 -0500

    Add files via upload

---
+++ arrays.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    // int marks[5]; //declaration
+    // marks[0] = 10;
+    // marks[1] = 20;
+    // marks[2] = 30;
+    // marks[3] = 40;
+    // marks[4] = 50;
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     printf("%d \n",marks[i]);
+    // }
+    // printf("%d",marks[2]);
+
+    int marks[5]={10,20,30,40,50};
+    printf("%d \n",marks[4]);
+
+    return 0;
+}
