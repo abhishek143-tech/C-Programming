662346884e797b9460a31b3a140c2fff500d213c
Abhishek Dilip Dhok <53620210+abhishek143-tech@users.noreply.github.com>
2021-01-19 05:21:54 -0500

    Add files via upload

---
+++ strings.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+    // char name[]={'a','b','h','i','\0'};
+    // printf("%s \n",name);
+
+    char arr[] = "subodh is good boy";
+    printf("%s \n",arr);
+    return 0;
+}
